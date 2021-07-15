d68e749eab9f1a34b7b4f8beb75e682fc83cf7d3
johnspeny <johnspeny@gmail.com>
2021-07-15 23:48:29 +0300

    Splash Screen Set Up

---
+++ app/src/main/java/com/okker/earlymarket/activities/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.okker.earlymarket.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.okker.earlymarket.MainActivity
+import com.okker.earlymarket.R
+
+class SplashScreen : AppCompatActivity() {
+
+    private val _SPLASHTIME: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, _SPLASHTIME)
+
+    }
+}
