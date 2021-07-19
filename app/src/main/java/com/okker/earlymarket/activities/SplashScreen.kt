8f35177ac319102901ac038d05759ed713288e6a
johnspeny <johnspeny@gmail.com>
2021-07-19 10:13:22 +0300

    Add Activity

--- app/src/main/java/com/okker/earlymarket/activities/SplashScreen.kt
+++ app/src/main/java/com/okker/earlymarket/activities/SplashScreen.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.okker.earlymarket.MainActivity
 import com.okker.earlymarket.R
 
 class SplashScreen : AppCompatActivity() {
