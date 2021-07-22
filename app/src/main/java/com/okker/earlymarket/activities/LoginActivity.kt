41736c7bf38a89788cd117a9ec058515181d517c
johnspeny <johnspeny@gmail.com>
2021-07-22 23:13:27 +0300

    Connect Login to Sign to Main

--- app/src/main/java/com/okker/earlymarket/activities/LoginActivity.kt
+++ app/src/main/java/com/okker/earlymarket/activities/LoginActivity.kt
@@ -1,13 +1,29 @@
 package com.okker.earlymarket.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.okker.earlymarket.R
+import com.okker.earlymarket.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.emSubmitLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        binding.emGotoSignup.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }
 
         supportActionBar?.hide()
     }
--- app/src/main/java/com/okker/earlymarket/activities/SignUpActivity.kt
+++ app/src/main/java/com/okker/earlymarket/activities/SignUpActivity.kt
@@ -1,15 +1,32 @@
 package com.okker.earlymarket.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.okker.earlymarket.R
+import com.okker.earlymarket.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         supportActionBar?.hide()
 
+        binding.emSubmitSignup.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        binding.emGotoLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
